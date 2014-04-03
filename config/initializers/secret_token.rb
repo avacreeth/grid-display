58e279f465e70ab77f5ab9c1f1981f1e9aa739b3
Andy Creeth <andy@yesware.com>
2014-04-03 14:55:46 -0400

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YetiGrid::Application.config.secret_token = 'c4600ebf98b6e330733fd113b5d465b0cd3d7991201331c283d6c7ad095a81394c20985fff08a7bb75866109fdea8e2f822764ef4f4a29c64f8a56c2b7f39cfe'
