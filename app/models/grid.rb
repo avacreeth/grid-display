330d7c901ee64f2653180eb9c3805c5c663a81d3
Andy Creeth <andy@yesware.com>
2014-04-03 15:14:28 -0400

    added relationships

--- app/models/grid.rb
+++ app/models/grid.rb
@@ -1,3 +1,4 @@
 class Grid < ActiveRecord::Base
   attr_accessible :name
+  has_many :tiles
 end
--- app/models/tile.rb
+++ app/models/tile.rb
@@ -1,3 +1,4 @@
 class Tile < ActiveRecord::Base
   attr_accessible :content, :grid_id, :type
+  belongs_to :grid
 end
